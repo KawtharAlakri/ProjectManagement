a5199823b163b308ce7539b2f1a22ea5b18c5706
KawtharAlakri <202003447@student.polytechnic.bh>
2023-05-19 01:03:57 +0300

    database injection

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ProjectManagementContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
