a4046b1c8dea98f3148979490a6f17f0ce185ccc
KawtharAlakri <202003447@student.polytechnic.bh>
2023-05-21 18:43:24 +0300

    identity added

--- ProjectManagement/Program.cs
+++ ProjectManagement/Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using ProjectManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using ProjectManagement.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using IdentityContext = ProjectManagement.Data.IdentityContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
-builder.Services.AddDbContext<ProjectManagementContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("IdentityDbContextConnection")
+    ));
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<ProjectManagement.Data.IdentityContext>();
 
 var app = builder.Build();
 
@@ -19,11 +29,12 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
